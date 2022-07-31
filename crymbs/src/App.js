0c1c297337c7b2f169f1c66cf26676251fc88177
Nkeiruka Whenu <nkewhenu@WKMZTD9C5683.global.publicisgroupe.net>
2022-07-31 02:56:05 +0100

    Minor progress. 'Able to sign in'

--- crymbs/src/App.js
+++ crymbs/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import logo from './crymbs.png';
 import './App.css';
+import GetUserCredentials from './Actions';
+import {useState} from 'react';
+// import {GetUsername, MyInventory} from './Actions'
+
+
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [walletID, setWalletID] = useState("");
+  const [signedIn, setLoginStatus] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Welcome to Crymbs </p>
       </header>
+      <body>
+        <div className="UserStats">Your Username: {username} : {walletID}</div>
+
+        <table>
+          <thead>
+            <tr>
+              <th className="yourHistory">
+                YOUR HISTORY
+              </th>
+              <th>
+                YOUR INVENTORY
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr className="header-row">
+              <td className="table-tab">
+                
+              </td>
+              <td className="table-tab">
+                In your inventory, you have...
+                {/* <MyInventory/> */}
+              </td>
+            </tr>
+            <tr>
+              <td className="table-tab">
+                <LoginDecisionHandler
+                  setUsername={setUsername}
+                  setWalletID={setWalletID}
+                  signedIn={signedIn}
+                  setLoginStatus={setLoginStatus}
+                />
+              </td>
+              <td className="table-tab">creation</td>
+            </tr>
+          </tbody>
+        </table>
+
+      </body>
     </div>
   );
+}
+
+function LoginDecisionHandler(props) {
+  if(!props.signedIn){
+    return (
+      <GetUserCredentials className="dispGetUsername"
+        setUsername={props.setUsername}
+        setWalletID={props.setWalletID}
+        signedIn={props.signedIn}
+        setLoginStatus={props.setLoginStatus}
+      />
+    )
+  } else {
+    return (
+      <div>test</div>
+    )
+  }
 }
 
 export default App;
